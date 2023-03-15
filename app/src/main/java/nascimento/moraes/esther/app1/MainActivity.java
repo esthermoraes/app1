329e80f6d4918895d215bb2e7f521b140c16cea0
Esther Moraes Nascimento <20211TIIMI0052@cefetes.br>
2023-03-15 08:36:06 -0300

    Initial commit

---
+++ app/src/main/java/nascimento/moraes/esther/app1/MainActivity.java
@@ -0,0 +1,29 @@
+package nascimento.moraes.esther.app1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btnEnviar = findViewById(R.id.btnEnviar);
+        btnEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText editText = findViewById(R.id.etDigiteAqui);
+                String textoDigitado = editText.getText().toString();
+                Intent i = new Intent(MainActivity.this,NextActivity.class);
+                i.putExtra("texto", textoDigitado);
+                startActivity(i);
+            }
+        });
+    }
+}
